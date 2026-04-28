22244c5bef19029fd3fc3f02d7ad237632bd3e3d
qobilshokirov2012-hash <qobilshokirov2012@gmail.com>
2026-04-28 20:21:11 +0500

    Update main.py

--- main.py
+++ main.py
@@ -1,26 +1,29 @@
 import asyncio
 import logging
+import sys
+import os
+
+# Root katalogni tanitish (Xatolikni oldini oladi)
+sys.path.append(os.path.dirname(os.path.abspath(__file__)))
+
 from aiogram import Bot, Dispatcher
 from config import BOT_TOKEN
 from database.db import init_db
-from handlers import start, search, sevimlilar
+from handlers import search, sevimlilar  # Bu ishlashi uchun handlers/__init__.py bo'lishi kerak
 
 logging.basicConfig(level=logging.INFO)
 
 async def main():
-    # MB yaratish
     await init_db()
     
     bot = Bot(token=BOT_TOKEN)
     dp = Dispatcher()
 
     # Routerlarni ulash
-    dp.include_routers(
-        search.router,
-        sevimlilar.router
-    )
+    dp.include_router(search.router)
+    dp.include_router(sevimlilar.router)
 
-    print("Bot ishga tushdi...")
+    logging.info("Bot ishga tushdi...")
     await dp.start_polling(bot)
 
 if __name__ == "__main__":
