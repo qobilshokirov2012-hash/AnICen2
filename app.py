fb141b2ac20ca822a34d13f42f2ef9d243126b84
qobilshokirov2012-hash <qobilshokirov2012@gmail.com>
2026-05-01 19:58:05 +0500

    Update app.py

--- app.py
+++ app.py
@@ -11,16 +11,43 @@ client = MongoClient(MONGO_URI)
 db = client['hikoyalar_db']
 stories_collection = db['stories']
 
+def to_cyrillic(text):
+    """Lotin alifbosini Kirillga o'girish"""
+    mapping = {
+        "sh": "ш", "Sh": "Ш", "ch": "ч", "Ch": "Ч",
+        "yo": "ё", "Yo": "Ё", "yu": "ю", "Yu": "Ю",
+        "ya": "я", "Ya": "Я", "o'": "ў", "O'": "Ў",
+        "g'": "ғ", "G'": "Ғ", "a": "а", "b": "б",
+        "d": "д", "e": "е", "f": "ф", "g": "г",
+        "h": "ҳ", "i": "и", "j": "ж", "k": "к",
+        "l": "л", "m": "м", "n": "н", "o": "о",
+        "p": "п", "q": "қ", "r": "р", "s": "с",
+        "t": "т", "u": "у", "v": "в", "x": "х",
+        "y": "й", "z": "з", "E": "Э", "e": "е"
+    }
+    res = text
+    for lat, cyr in mapping.items():
+        res = res.replace(lat, cyr)
+    return res
+
 @app.route('/')
 def index():
-    # Asosiy sahifaga kirganda admin panelga yo'naltiramiz yoki hikoyalar ro'yxatiga
     return redirect(url_for('admin'))
 
 @app.route('/story/<story_id>')
 def view_story(story_id):
-    story = stories_collection.find_one({'_id': ObjectId(story_id)})
-    if not story:
+    story_db = stories_collection.find_one({'_id': ObjectId(story_id)})
+    if not story_db:
         return "Hikoya topilmadi", 404
+    
+    # Jinja2 uchun 'story' obyektini shakllantiramiz
+    story = {
+        "title_lat": story_db['title'],
+        "title_cyr": to_cyrillic(story_db['title']),
+        "content_lat": story_db['content'],
+        "content_cyr": to_cyrillic(story_db['content'])
+    }
+    
     return render_template('story.html', story=story)
 
 @app.route('/admin', methods=['GET', 'POST'])
