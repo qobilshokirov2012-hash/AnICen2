b200063aadcf74e8df8aae8b6e11a8199fe61880
qobilshokirov2012-hash <qobilshokirov2012@gmail.com>
2026-05-01 18:48:33 +0500

    Delete database directory

--- database/db.py
+++
@@ -1,6 +0,0 @@
-# Oldin: from config import DATABASE_NAME
-# Keyin (xavfsizroq yo'l):
-import sys
-import os
-sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
-from config import DATABASE_NAME
